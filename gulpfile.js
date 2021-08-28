890e8446c4c4da86a31eaab74e6ad242f3485053
DanielSDVG <danielsdvg@gmail.com>
2021-08-28 21:36:38 +0200

    Created stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp.src('./sass/!(_*).(scss|sass)')
+  return gulp.src('./sass/!(_*).@(scss|sass)')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 };
