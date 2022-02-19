b0e55eb04ec5cb318877c3dfae4f5a7f1ad2fdfe
DanielSDVG <daniperezporras@gmail.com>
2022-02-19 10:57:55 +0100

    recompile stylesheets and example documents

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return gulp.src('./sass/[!_]*.@(scss|sass)')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./out/css'));
+    .pipe(gulp.dest('./examples/out/css'));
 };
 
 exports.sass = buildStyles;
